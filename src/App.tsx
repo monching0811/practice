e9f156b560db3e4f61b2e9aadc29320562329c4a
monching0811 <almondgugulan081100@gmail.com>
2025-11-28 19:16:29 +0800

    try

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"; // <-- Import the Shadcn Button
+
+function App() {
+  return (
+    // Use Tailwind classes to center the content
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Fuck me</Button>
+    </div>
+  );
+}
+
+export default App;
